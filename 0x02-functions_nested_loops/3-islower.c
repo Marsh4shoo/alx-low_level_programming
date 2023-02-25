47f77b7f3d4c69dc5bf8d17273b24de00eb76d0b
Marsh4shoo <marshal.wjnr@gmail.com>
2023-02-25 13:55:54 -0800

    correcting 3-inlower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,17 +1,16 @@
-
 #include "main.h"
-/**
-* 
+/** 
 * _islower - this prints in lowercase if within the stated ranges
+*
 * @c: is this is the parameter to printed
 *
 * Return: 1 if in lowercas and 0 if otherwise
 */
 
-	int _islower(int c)
+int _islower(int c)
 {
-		if (c >= 97 && c <= 122)
-			return (1);
-		else
-			return (0);
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
 }
