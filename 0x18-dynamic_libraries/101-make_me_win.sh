02b14eb62a33d605e11d7e9ccc011340da12af0c
Marsh4shoo <marshal.wjnr@gmail.com>
2023-08-24 13:00:18 -0700

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 wget -P /tmp/ https://github.com/Marsh4shoo/alx-low_level_programming/blob/master/0x18-dynamic_libraries/nrandom.so
 export LD_PRELOAD=/tmp/nrandom.so
+./gm 9 8 10 24 75 9
